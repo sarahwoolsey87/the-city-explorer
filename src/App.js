90ed919dd84d9ae0d5b1fc3c5b8a13bc7fe48480
sarahwoolsey87 <shedontusejelly0@gmail.com>
2023-07-24 16:00:22 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,49 +1,59 @@
 import { useState } from "react";
 import "./App.css";
 
-import axios from "axios";
-
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [location, setLocation] = useState({});
   const [map, setMap] = useState("");
 
   function handleSearch(event) {
-
     setSearchQuery(event.target.value);
-    console.log(searchQuery);
   }
 
   async function getLocation(event) {
     try {
       event.preventDefault();
       event.target.input.value = "";
-      
+  
       const API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json`;
-      const res = await axios.get(API);
-      console.log(res.data[0]);
-      setLocation(res.data[0]);
-      handleMap(res.data[0]);
+      const res = await fetch(API);
+      const data = await res.json();
+      
+      if (data && data.length > 0) {
+        setLocation(data[0]);
+        handleMap(data[0]);
+      } else {
+        console.log("No location data found.");
+      }
     } catch (error) {
       console.log(error);
     }
   }
 
   function handleMap(data) {
-    
-    const API = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${data.lat},${data.lon}&zoom=9`;
-    
-    setMap(API);
+    const mapURL = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${data.lat},${data.lon}&zoom=12`;
+    setMap(mapURL);
   }
 
   return (
     <div className="App">
       <form onSubmit={getLocation}>
-        <input type="text" placeholder="search for a city" name="input" onChange={handleSearch} />
+        <input
+          type="text"
+          placeholder="search for a city"
+          name="input"
+          onChange={handleSearch}
+        />
         <button type="submit">Explore!</button>
       </form>
-      <p>{location.display_name}</p>
-      {map && <img src={map} alt="map" />}
+      <div className="map-container">
+        {map && <img src={map} alt="map" />}
+      </div>
+      <div className="location-info">
+        <p>{location.display_name}</p>
+        <p>Latitude: {location.lat}</p>
+        <p>Longitude: {location.lon}</p>
+      </div>
     </div>
   );
 }
